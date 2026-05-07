8f194e73f05adae9eddc36e5691b485197cbadc5
Santiago Silva <santimortal02@gmail.com>
2026-05-07 15:27:22 -0300

    prueba1

--- funciones.py
+++ funciones.py
@@ -1,2 +1,32 @@
-def saludo(nombre):
-    print(f"Hola {nombre}!")
+import csv
+def menu():
+   while True:
+    opcion = input("----MENU----\n1. Mostrar paises\n:")
+    while not opcion.isdigit():
+       opcion = input("Ingrese un numero valido!")
+    opcion = int(opcion)   
+    match opcion:
+       case 1:
+          mostrar()
+       case _:
+          print("Ingrese una opcion valida!")   
+def mostrar():
+ paises = []
+ with open("paises.csv", encoding="utf-8") as archivo:
+     lector = csv.DictReader(archivo)
+     for fila in lector:
+         pais = {
+            "nombre": fila["nombre"],
+            "poblacion": int(fila["poblacion"]),
+            "superficie": int(fila["superficie"]),
+            "continente": fila["continente"]
+         }
+
+         paises.append(pais)
+
+ for pais in paises:
+    print("Nombre:", pais["nombre"])
+    print("Población:", pais["poblacion"])
+    print("Superficie:", pais["superficie"])
+    print("Continente:", pais["continente"])
+    print()
