cded07fbf7d4bcdbd16805a5264771e3b6ccd9e8
Santiago Silva <santimortal02@gmail.com>
2026-05-19 15:04:25 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -2,12 +2,14 @@ import csv
 import unicodedata
 from statistics import mean
 ######## Validaciones ########
+#Validacion de texto
 def validarletras(texto):
   texto = texto.strip()
   if not texto.isalpha():
     return False
   return texto.isalpha
 ##############################
+#Validacion de enteros
 def validarnumint(numero):
   try:
     numero = int(numero)
@@ -19,6 +21,7 @@ def validarnumint(numero):
     print("Error: Ingrese solamente numeros enteros!")
     return False
 ##############################
+#Validacion de paises, retorna una tupla con el valor sobre si lo encontro, su posicion, y la lista de diccionarios actualizada
 def validarpais(nombre):
   with open("Paises.csv", "r", encoding="utf-8") as archivo:
      lector = csv.DictReader(archivo)
@@ -28,6 +31,7 @@ def validarpais(nombre):
          return True, posicion, filas
      return False, -1, filas
 ##############################
+#Toma palabras y las retorna sin tildes
 def quitartildes(texto):
 
     texto = unicodedata.normalize("NFD", texto)
@@ -38,7 +42,9 @@ def quitartildes(texto):
     )
 
     return texto 
-############################## 
+##############################
+#Mediante una lista preestablecida de continentes , comparamos si el ingresado por el usuario es valido o no
+#y retornamos una tupla con el valor sobre si es o no valido, el lugar pero escrito correctamente, y la lista de diccionarios actualizada  
 def validarcontinente(lugar):
   with open("Paises.csv", "r", encoding="utf-8") as archivo:
      lector = csv.DictReader(archivo)
@@ -48,13 +54,17 @@ def validarcontinente(lugar):
     if quitartildes(lugar).lower() == quitartildes(continentes[i]).lower():
       lugar = continentes[i]
       return True, lugar, filas
-  return False, lugar, filas     
-###########Procesos############    
+  return False, lugar, filas
+#######################################     
+###########Procesos############
+#Importamos la lista de diccionarios del CSV     
 def listafilas():
   with open("Paises.csv", "r", encoding="utf-8") as archivo:
      lector = csv.DictReader(archivo)
      filas = list(lector)
      return filas
+##########################################  
+#Escribimos o sobrescribimos una fila entera del archivo CSV
 def modificar(filas):
   with open("Paises.csv", "w", newline="", encoding="utf-8") as archivo:
 
@@ -65,6 +75,9 @@ def modificar(filas):
         escritor.writeheader()
 
         escritor.writerows(filas)
+#######################################
+#Tomamos el metodo de orden(ordenado) y la forma en la que se requiera, ascendente o descendente(invertido).
+#Luego mostramos la lista de diccionarios ordenada segun el metodo
 def ordenamiento(ordenado,invertido):
   filas = listafilas()
   if ordenado == 'poblacion' or ordenado == 'superficie':
@@ -74,6 +87,8 @@ def ordenamiento(ordenado,invertido):
   for fila in filas:
     print(f"Pais: {fila['nombre']}\nPoblacion: {fila['poblacion']}\nSuperficie: {fila['superficie']}\nContinente: {fila['continente']}\n------------------------")       
 #####Opcion 1#####
+#Solicitamos al usuario un nombre,continente,superficie y poblacion validamos mediante las funciones de arriba
+#Y luego las agregamos al archivo CSV siguiendo el formato
 def agregar():
   while True: 
    nombre = input("Ingrese el nombre del pais: ")
@@ -104,7 +119,9 @@ def agregar():
      escritor.writerow(pais)
    print("\nPais agregado correctamente\n")
    return pais
-#####Opcion 2##### 
+#####Opcion 2#####
+#El usuario ingresa el pais que desea cambiar y se valida mediante las funciones de arriba
+#En el caso de que lo encuentre se solicita los numeros a cambios que tambien son validados 
 def actualizar():
  while True: 
   with open("Paises.csv", "r", encoding="utf-8") as archivo:
@@ -137,6 +154,10 @@ def actualizar():
     print("Error: Ingrese un nombre valido!")
     break
 ###########################################
+#####Opcion 3#####
+#Se valida si el pais que desea buscar el usuario se encuentra mediante las funciones de arriba y luego
+#El programa toma valido abreviaciones tambien, como por ejemplo de América, amer.
+#En el caso de que retorne verdadero, lo muestra con toda su informacion
 def buscar():
   while True:
    busqueda = input("Ingrese el pais que desea buscar: ")
@@ -157,6 +178,10 @@ def buscar():
        break
      break 
 ###################
+#####Opcion 4#####
+#El usuario elige la manera de filtro y luego
+#Si elige filtrar por continentes este validara si el continente existe y luego recorrera la lista mostrando todos los archivos de este mismo continente
+#Si elige rango de poblacion o superficie, este recorrera la lista mostrando los paises que esten dentro de ese rango, el cual fue establecido por el usuario
 def filtrar():
  while True:
   filtro = input("Filtrar paises por:\n1. Continente\n2. Rango de poblacion\n3. Rango de superficie")
@@ -222,6 +247,10 @@ def filtrar():
     case _:
       print("Error: Ingrese una opcion entre 1-3!")
       break
+##########################
+#####Opcion 5#####
+#Segun la manera que decida ordenar el usuario es la informacion que envia a la funcion ordenar
+#Esta funcion solamente envia la forma en la que se debe ordenar la lista y si debe ser de manera ascendente o descendente
 def ordenar():
  while True:
   orden = input("Ingrese de que manera desea ordenar los paises:\n1. Nombre\n2. Poblacion\n3. Superficie\n:")
@@ -254,6 +283,8 @@ def ordenar():
     case _:
       print("Error: Ingrese una opcion entre 1-2!")
       break
+##################################
+#####Opcion 6######
 def estadistica():
   while True:
    calculado = input("Ingrese la opcion que desea:\n1. Pais con mayor poblacion\n2. Pais con menor poblacion\n3. Promedio de poblacion\n4. Promedio de superficie\n5. Cantidad de paises por continente\n:")
@@ -263,37 +294,54 @@ def estadistica():
    calculado = int(calculado)
    filas = listafilas()
    match calculado:
+     #Se recorren todas las filas de la lista usando la funcion max y mostramos el mayor valor
      case 1:
 
       mayor = max(filas, key=lambda x:int(x['poblacion']))
       for clave, valor in mayor.items():
        print(f"{clave.title()}: {valor}")
       print("----------------------")
-      break 
+      break
+     #Se recorren todas las filas de la lista usando la funcion min y mostramos el menor valor 
      case 2: 
       menor = min(filas, key=lambda x:int(x['poblacion']))
       for clave, valor in menor.items():
        print(f"{clave.title()}: {valor}")
       print("----------------------")
       break
+     #Usando la funcion mean importada de statistics sumamos todos los valores de poblacion y sacamos un promedio
+     #Estos valores los obtenemos recorriendo la lista con un for
      case 3:
        poblacionsumada = mean([int(x['poblacion']) for x in filas])
        print(f"El promedio de poblacion es de {poblacionsumada} por pais\n-------------------")
        break
+     #Exactamente lo mismo que recien, solamente que con superficie
      case 4:
        superficiesumada = mean([int(x['superficie']) for x in filas])
        print(f"El promedio de superficie es de {superficiesumada} por pais\n-------------------")
        break
+     # Creamos un diccionario vacío llamado "continentes". Este diccionario almacenará:
+     # clave   -> nombre del continente
+     # valor   -> cantidad de países de ese continente
      case 5:
       continentes = {}
+      # Recorremos cada país de la lista "filas".
+      # Cada "fila" es un diccionario con los datos de un país.
       for fila in filas:
           continente = fila["continente"]
+          # Verificamos si ese continente ya existe
+          # dentro del diccionario "continentes".
           if continente in continentes:
               continentes[continente] += 1
+          # Si NO existe, creamos la clave y comenzamos en 1.    
           else:
               continentes[continente] = 1
       print("Cantidad de paises por continente")
       print("--------------------------------")
+      # Recorremos el diccionario final usando .items()
+      # para obtener:
+      # continente -> clave
+      # cantidad   -> valor
       for continente, cantidad in continentes.items():
           print(f"{continente}: {cantidad}")
       print("--------------------------------")
